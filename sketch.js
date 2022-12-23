7022da70620a7561fe3231bd8e26722fdcb02903
Jack <jack.morgan.edwards@gmail.com>
2022-12-23 11:52:28 -0500

    initial commit

---
+++ sketch.js
@@ -0,0 +1,56 @@
+let inc = 0.1;
+let scl = 20;
+let cols, rows;
+
+let zoff = 0;
+
+let particles = [];
+
+let flowfield;
+
+function setup() {
+  createCanvas(1600, 1000);
+
+  cols = floor(width / scl);
+  rows = floor(height / scl);
+
+  flowfield = new Array(cols * rows);
+
+  for (let i = 0; i < 1000; i++) {
+    particles[i] = new Particle();
+  }
+
+  background(0);
+}
+
+function draw() {
+  let yoff = 0;
+  for (let y = 0; y < rows; y++) {
+    let xoff = 0;
+    for (let x = 0; x < cols; x++) {
+      let index = x + y * cols;
+      let angle = noise(xoff, yoff, zoff) * TWO_PI;
+      let v = p5.Vector.fromAngle(angle);
+      v.setMag(0.5);
+      flowfield[index] = v;
+      xoff += inc;
+      stroke(0, 50);
+      push();
+      translate(x * scl, y * scl);
+      rotate(v.heading());
+      strokeWeight(1);
+      // line(0, 0, scl, 0);
+      pop();
+    }
+    yoff += inc;
+
+    zoff += 0.001;
+  }
+
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].follow(flowfield);
+    particles[i].edges();
+    particles[i].show();
+    particles[i].update();
+  }
+}
